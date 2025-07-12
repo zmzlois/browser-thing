18ec365ab06c4dacb0a70059a6d5e29b52a3ecc2
Nicolas Ettlin <nicolas.ettlin@me.com>
2025-07-12 16:48:49 -0700

    Use Stagehand to fill out the form

--- mcp/playwright/browser.ts
+++ mcp/playwright/browser.ts
@@ -1,9 +1,21 @@
 import { chromium } from "playwright";
+import { Stagehand } from "@browserbasehq/stagehand";
 
-const browser = await chromium.launch();
-const context = await browser.newContext();
-const page = await context.newPage();
+const stagehand = new Stagehand({
+  env: "LOCAL",
+  modelName: "openai/gpt-4.1-mini",
+  modelClientOptions: {
+    apiKey: process.env.OPENAI_API_KEY,
+  },
 
+  localBrowserLaunchOptions: {
+    viewport: undefined, // TODO Set to null
+  },
+});
+
+await stagehand.init();
+
+const { page } = stagehand;
 await page.goto("http://localhost:5173");
 
-export { page, browser };
+export { stagehand, page };
